198cb05b7191df82199e49afc057d3ec92dc9b2b
DoubleYouW321 <grigorijbatisev739@gmail.com>
2025-06-09 15:08:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-gfrom flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 from data import db_session
